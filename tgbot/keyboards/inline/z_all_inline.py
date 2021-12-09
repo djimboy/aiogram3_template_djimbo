a4b214e2d4ccd81eeeefa63177baaecd8378ba24
Djimbo <djimbosfan@mail.ru>
2021-12-09 16:20:50 +0300

    Update z_all_inline.py

--- tgbot/keyboards/inline/z_all_inline.py
+++ tgbot/keyboards/inline/z_all_inline.py
@@ -1,1 +1,12 @@
+# - *- coding: utf- 8 - *-
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+from tgbot.utils.free_functions import ikb
+
+test_inl = InlineKeyboardBuilder(
+).row(
+    ikb("Test 1", data="..."),
+    ikb("Test 2", data="..."),
+).row(
+    ikb("Test 3", data="..."),
+).as_markup()
