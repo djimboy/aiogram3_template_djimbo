463ce26c3606e416238046adefef7eedf3053c86
Djimbo <djimbosfan@mail.ru>
2021-12-10 04:24:04 +0300

    Update throttling.py

--- tgbot/middlewares/throttling.py
+++ tgbot/middlewares/throttling.py
@@ -13,21 +13,16 @@ class ThrottlingMiddleware(BaseMiddleware):
         self.last_throttled = 0
 
     async def __call__(self, handler, event: types.Message, data):
-        user: Optional[User] = data.get("event_from_user")
-
-        if user:
-            if int(time.time()) - self.last_throttled >= self.default_rate:
-                self.last_throttled = int(time.time())
-                self.default_rate = 0.5
-                self.count_throttled = 0
-                return await handler(event, data)
-            else:
-                if self.count_throttled >= 2:
-                    self.default_rate = 3
-                else:
-                    self.count_throttled += 1
-                    await event.reply("<b>❗ Please, do not spam.</b>")
-
+        if int(time.time()) - self.last_throttled >= self.default_rate:
             self.last_throttled = int(time.time())
-        else:
+            self.default_rate = 0.5
+            self.count_throttled = 0
             return await handler(event, data)
+        else:
+            if self.count_throttled >= 2:
+                self.default_rate = 3
+            else:
+                self.count_throttled += 1
+                await event.reply("<b>❗ Please, do not spam.</b>")
+
+        self.last_throttled = int(time.time())
