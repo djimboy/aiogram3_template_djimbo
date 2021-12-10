b2f86943968ba00141b6814289ffe2a1aad17774
Djimbo <djimbosfan@mail.ru>
2021-12-10 11:56:41 +0300

    Update z_all_reply.py

--- tgbot/keyboards/reply/z_all_reply.py
+++ tgbot/keyboards/reply/z_all_reply.py
@@ -12,7 +12,7 @@ def menu_frep(user_id):
         rkb("User 1"), rkb("User 2"), rkb("User 3")
     )
 
-    if str(user_id) in get_admins():
+    if user_id in get_admins():
         keyboard.row(rkb("Admin 1"), rkb("Admin 2"), rkb("Admin 3"))
 
     return keyboard.as_markup(resize_keyboard=True)
