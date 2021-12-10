7978791fe4250dfcd621d4273760c47937cc801e
Djimbo <djimbosfan@mail.ru>
2021-12-10 09:32:35 +0300

    Update __init__.py

--- tgbot/filters/__init__.py
+++ tgbot/filters/__init__.py
@@ -1,8 +1,1 @@
-# - *- coding: utf- 8 - *-
-from aiogram import Router
 
-from tgbot.filters.all_filters import IsPrivate
-
-
-def setup_filters(router: Router):
-    router.message.filter(IsPrivate())
