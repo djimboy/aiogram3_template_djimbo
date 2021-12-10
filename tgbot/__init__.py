d1207f5cacd028e3bc81b8f22fc7d30c8e55febe
Djimbo <djimbosfan@mail.ru>
2021-12-10 09:37:44 +0300

    Update __init__.py

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -1,12 +1,1 @@
-# - *- coding: utf- 8 - *-
-from aiogram import Router
 
-from tgbot.filters import setup_filters
-from tgbot.handlers import setup_handlers
-from tgbot.middlewares import setup_middlwares
-
-
-def setup_events(router: Router):
-    setup_middlwares(router)
-    setup_filters(router)
-    setup_handlers(router)
