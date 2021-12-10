61154e638b2d828327b37603e72716f57dcacd4d
Djimbo <djimbosfan@mail.ru>
2021-12-10 09:23:29 +0300

    Create __init__.py

---
+++ tgbot/handlers/user/__init__.py
@@ -0,0 +1,14 @@
+# - *- coding: utf- 8 - *-
+from aiogram import Router
+
+from tgbot.handlers.main_start import register_main_start
+from tgbot.handlers.user.user_menu import register_user_menu
+from tgbot.handlers.z_all_errors import register_all_errors
+from tgbot.handlers.z_all_missed import register_all_missed
+
+
+def setup_user_handlers(router: Router):
+    register_all_errors(router)
+    register_main_start(router)
+    register_user_menu(router)
+    register_all_missed(router)
