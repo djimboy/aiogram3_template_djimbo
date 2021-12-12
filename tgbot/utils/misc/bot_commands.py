b7d8781095b622255d7018d555635970fc6ae086
Djimbo <djimbosfan@mail.ru>
2021-12-12 15:18:29 +0300

    Update bot_commands.py

--- tgbot/utils/misc/bot_commands.py
+++ tgbot/utils/misc/bot_commands.py
@@ -6,12 +6,12 @@ from tgbot.config import get_admins
 
 # Commands for users
 default_commands = [
-    types.BotCommand(command="start", description="♻ Restart the bot"),
+    types.BotCommand(command="start", description="♻ Перезапустить бота"),
 ]
 
 # Commands for admins
 admin_commands = [
-    types.BotCommand(command="start", description="♻ Restart the bot"),
+    types.BotCommand(command="start", description="♻ Перезапустить бота"),
 ]
 
 
