c7d2b5e1dd69a1c90a8cee6ed75362fe9a144dbb
Djimbo <djimbosfan@mail.ru>
2021-12-13 16:28:47 +0300

    Update bot_logging.py

--- tgbot/utils/misc/bot_logging.py
+++ tgbot/utils/misc/bot_logging.py
@@ -2,19 +2,27 @@
 import logging
 
 
+from colorlog import ColoredFormatter
+
+
 # Build logging
 def start_logging():
-    log_formatter = logging.Formatter(u"[%(levelname)s] [%(asctime)s] | [%(filename)s LINE:%(lineno)d] | %(message)s")
+    log_formatter_file = logging.Formatter(u"[%(levelname)s] [%(asctime)s] | [%(filename)s LINE:%(lineno)d] | %(message)s")
+    log_formatter_console = ColoredFormatter(
+        "%(cyan)s%(asctime)s %(blue)s| %(log_color)s%(levelname)s %(blue)s"
+        "| %(purple)s%(filename)s:%(lineno)d %(blue)s| %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+    )
 
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.INFO)
 
     file_handler = logging.FileHandler("logs.log", "w", "utf-8")
-    file_handler.setFormatter(log_formatter)
+    file_handler.setFormatter(log_formatter_file)
     file_handler.setLevel(logging.INFO)
 
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
+    console_handler.setFormatter(log_formatter_console)
     console_handler.setLevel(logging.ERROR)
 
     root_logger.addHandler(file_handler)
