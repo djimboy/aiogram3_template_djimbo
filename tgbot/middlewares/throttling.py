093a61499945b81b2ff55cca77af20b38061429d
Djimbo <djimbosfan@mail.ru>
2021-12-18 21:15:59 +0300

    Update throttling.py

--- tgbot/middlewares/throttling.py
+++ tgbot/middlewares/throttling.py
@@ -2,25 +2,37 @@
 import time
 
 from aiogram import BaseMiddleware, types
+from aiogram.dispatcher.event.handler import HandlerObject
 
 
 class ThrottlingMiddleware(BaseMiddleware):
-    def __init__(self, default_rate: int = 0.5) -> None:
+    def __init__(self, sd=False, default_rate: int = 0.5) -> None:
+        self.limiters = {}
+
         self.default_rate = default_rate
         self.count_throttled = 1
         self.last_throttled = 0
 
     async def __call__(self, handler, event: types.Message, data):
-        if int(time.time()) - self.last_throttled >= self.default_rate:
-            self.last_throttled = int(time.time())
-            self.default_rate = 0.5
-            self.count_throttled = 0
-            return await handler(event, data)
-        else:
-            if self.count_throttled >= 2:
-                self.default_rate = 3
-            else:
-                self.count_throttled += 1
-                await event.reply("<b>❗ Пожалуйста, не спамьте.</b>")
+        real_handler: HandlerObject = data["handler"]
+        skip_pass = True
 
-        self.last_throttled = int(time.time())
+        if real_handler.flags.get("skip_pass") is not None:
+            skip_pass = real_handler.flags.get("skip_pass")
+
+        if skip_pass:
+            if int(time.time()) - self.last_throttled >= self.default_rate:
+                self.last_throttled = int(time.time())
+                self.default_rate = 0.5
+                self.count_throttled = 0
+                return await handler(event, data)
+            else:
+                if self.count_throttled >= 2:
+                    self.default_rate = 3
+                else:
+                    self.count_throttled += 1
+                    await event.reply("<b>❗ Пожалуйста, не спамьте.</b>")
+
+            self.last_throttled = int(time.time())
+        else:
+            return await handler(event, data)
