a8a22a3010224d16a7dc42bfc069a2206be2bc0f
Djimbo <djimbosfan@mail.ru>
2022-01-08 14:10:03 +0300

    Update api_sqlite.py

--- tgbot/services/api_sqlite.py
+++ tgbot/services/api_sqlite.py
@@ -20,6 +20,9 @@ def dict_factory(cursor, row):
 ####################################### FORMATTING REQUESTS ########################################
 # Formatting a request with arguments
 def update_format_with_args(sql, parameters: dict):
+    if "XXX" not in sql:
+        sql += " XXX "
+
     values = ", ".join([
         f"{item} = ?" for item in parameters
     ])
@@ -31,6 +34,7 @@ def update_format_with_args(sql, parameters: dict):
 # Formatting a request without arguments
 def get_format_args(sql, parameters: dict):
     sql = f"{sql} WHERE "
+
     sql += " AND ".join([
         f"{item} = ?" for item in parameters
     ])
@@ -41,13 +45,13 @@ def get_format_args(sql, parameters: dict):
 ######################################### DATABASE REQUESTS ########################################
 ####################################################################################################
 # Add user
-def add_userx(user_id, user_login, user_name):
+def add_userx(user_id, user_login, user_name, user_surname):
     with sqlite3.connect(database_path) as con:
         con.row_factory = dict_factory
         con.execute("INSERT INTO storage_users "
-                    "(user_id, user_login, user_name, user_date, user_unix) "
-                    "VALUES (?, ?, ?, ?, ?)",
-                    [user_id, user_login, user_name, get_date(), get_unix()])
+                    "(user_id, user_login, user_name, user_surname, user_date, user_unix) "
+                    "VALUES (?, ?, ?, ?, ?, ?)",
+                    [user_id, user_login, user_name, user_surname, get_date(), get_unix()])
         con.commit()
 
 
@@ -81,9 +85,10 @@ def get_all_usersx():
 def update_userx(user_id, **kwargs):
     with sqlite3.connect(database_path) as con:
         con.row_factory = dict_factory
-        sql = f"UPDATE storage_users SET XXX WHERE user_id = {user_id}"
+        sql = f"UPDATE storage_users SET"
         sql, parameters = update_format_with_args(sql, kwargs)
-        con.execute(sql, parameters)
+        parameters.append(user_id)
+        con.execute(sql + "WHERE user_id = ?", parameters)
         con.commit()
 
 
@@ -106,12 +111,12 @@ def create_bdx():
         # Table with user data storage
         check_sql = con.execute("PRAGMA table_info(storage_users)").fetchall()
         check_create_users = [c for c in check_sql]
-        if len(check_create_users) == 6:
+        if len(check_create_users) == 7:
             print("DB was found(1/1)")
         else:
             con.execute("CREATE TABLE storage_users("
                         "increment INTEGER PRIMARY KEY AUTOINCREMENT, "
                         "user_id INTEGER, user_login TEXT, user_name TEXT, "
-                        "user_date TIMESTAMP, user_unix INTEGER)")
+                        "user_surname TEXT, user_date TIMESTAMP, user_unix INTEGER)")
             con.commit()
             print("DB was not found(1/1) | Creating...")
