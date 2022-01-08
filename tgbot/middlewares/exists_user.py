a824e2e4cc5c2e4777993331721dbcd9edf462b8
Djimbo <djimbosfan@mail.ru>
2022-01-08 14:10:33 +0300

    Update exists_user.py

--- tgbot/middlewares/exists_user.py
+++ tgbot/middlewares/exists_user.py
@@ -13,6 +13,7 @@ class ExistsUserMiddleware(BaseMiddleware):
             user_id = get_user.id
             user_login = get_user.username
             user_name = clear_html(get_user.first_name)
+            user_surname = clear_html(get_user.last_name)
 
             get_user_id = get_userx(user_id=user_id)
 
@@ -20,15 +21,17 @@ class ExistsUserMiddleware(BaseMiddleware):
                 if user_login is not None:
                     get_user_login = get_userx(user_login=user_login)
                     if get_user_login is None:
-                        add_userx(user_id, user_login.lower(), user_name)
+                        add_userx(user_id, user_login.lower(), user_name, user_surname)
                     else:
                         delete_userx(user_login=user_login)
-                        add_userx(user_id, user_login.lower(), user_name)
+                        add_userx(user_id, user_login.lower(), user_name, user_surname)
                 else:
-                    add_userx(user_id, user_login, user_name)
+                    add_userx(user_id, user_login, user_name, user_surname)
             else:
                 if user_name != get_user_id['user_name']:
                     update_userx(get_user_id['user_id'], user_name=user_name)
+                if user_surname != get_user_id['user_surname']:
+                    update_userx(get_user_id['user_id'], user_surname=user_surname)
                 if user_login is not None:
                     if user_login.lower() != get_user_id['user_login']:
                         update_userx(get_user_id['user_id'], user_login=user_login.lower())
