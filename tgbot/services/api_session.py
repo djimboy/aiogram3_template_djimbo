949b64a72160e4448abc61a4f9f7ffc9888b5167
Djimbo <djimbosfan@mail.ru>
2022-03-03 00:35:18 +0300

    Update api_session.py

--- tgbot/services/api_session.py
+++ tgbot/services/api_session.py
@@ -4,6 +4,7 @@ from typing import Optional
 import aiohttp
 
 
+# Asyncio session for requests
 class RequestsSession:
     def __init__(self) -> None:
         self._session: Optional[aiohttp.ClientSession] = None
