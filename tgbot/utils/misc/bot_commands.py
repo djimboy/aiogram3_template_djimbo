d361f3885aa9d9aeffba1d50d20179ad30339d4b
Djimbo <djimbosfan@mail.ru>
2022-05-04 23:14:56 +0300

    Update bot_commands.py

--- tgbot/utils/misc/bot_commands.py
+++ tgbot/utils/misc/bot_commands.py
@@ -14,6 +14,8 @@ user_commands = [
 admin_commands = [
     BotCommand(command="start", description="♻ Перезапустить бота"),
     BotCommand(command="inline", description="🌀 Получить Inline клавиатуру"),
+    BotCommand(command="log", description="🖨 Получить Логи"),
+    BotCommand(command="db", description="📦 Получить Базу Данных"),
 ]
 
 
@@ -22,4 +24,7 @@ async def set_commands(bot: Bot):
     await bot.set_my_commands(user_commands, scope=BotCommandScopeDefault())
 
     for admin in get_admins():
-        await bot.set_my_commands(admin_commands, scope=BotCommandScopeChat(chat_id=admin))
+        try:
+            await bot.set_my_commands(admin_commands, scope=BotCommandScopeChat(chat_id=admin))
+        except:
+            pass
