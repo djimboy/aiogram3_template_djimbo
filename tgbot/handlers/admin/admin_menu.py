9631bf7eba62acde25de724fa24dfc8d9606ec2d
Djimbo <djimbosfan@mail.ru>
2022-05-04 23:18:45 +0300

    Update admin_menu.py

--- tgbot/handlers/admin/admin_menu.py
+++ tgbot/handlers/admin/admin_menu.py
@@ -3,13 +3,16 @@ from aiogram import types, Router, Bot
 from aiogram.dispatcher.fsm.context import FSMContext
 from aiogram.types import FSInputFile
 
-from tgbot.config import DATABASE_PATH
-from tgbot.keyboards.inline import test_inl
-from tgbot.keyboards.reply import test_rep
+from tgbot.config import PATH_DATABASE, PATH_LOGS
+from tgbot.keyboards.z_all_inline import test_inl
+from tgbot.keyboards.z_all_reply import test_rep
 from tgbot.utils.const_functions import get_date
+
+router_admin_menu = Router()
 
 
 # Кнопка - Admin Inline
+@router_admin_menu.message(text="Admin Inline")
 async def admin_button_inline(message: types.Message, bot: Bot, rSession, state: FSMContext):
     await state.clear()
 
@@ -17,6 +20,7 @@ async def admin_button_inline(message: types.Message, bot: Bot, rSession, state:
 
 
 # Кнопка - Admin Reply
+@router_admin_menu.message(text="Admin Reply")
 async def admin_button_reply(message: types.Message, bot: Bot, rSession, state: FSMContext):
     await state.clear()
 
@@ -24,25 +28,19 @@ async def admin_button_reply(message: types.Message, bot: Bot, rSession, state: 
 
 
 # Получение Базы Данных
+@router_admin_menu.message(commands=['db', 'database'])
 async def admin_database(message: types.Message, bot: Bot, rSession, state: FSMContext):
     await state.clear()
 
-    await message.answer_document(FSInputFile(DATABASE_PATH),
+    await message.answer_document(FSInputFile(PATH_DATABASE),
                                   caption=f"<b>📦 BACKUP</b>\n"
                                           f"<code>🕰 {get_date()}</code>")
 
 
 # Получение логов
+@router_admin_menu.message(commands=['log', 'logs'])
 async def admin_log(message: types.Message, bot: Bot, rSession, state: FSMContext):
     await state.clear()
 
-    await message.answer_document(FSInputFile("logs.log"),
+    await message.answer_document(FSInputFile(PATH_LOGS),
                                   caption=f"<code>🕰 {get_date()}</code>")
-
-
-def register_admin_menu(router: Router):
-    router.message.register(admin_button_inline, text="Admin Inline", state="*")
-    router.message.register(admin_button_reply, text="Admin Reply", state="*")
-
-    router.message.register(admin_database, commands="db", state="*")
-    router.message.register(admin_log, commands="log", state="*")
