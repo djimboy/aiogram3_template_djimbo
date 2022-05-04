d05728b81fe65f5ae86ac5c97ad461432b7dd7f2
Djimbo <djimbosfan@mail.ru>
2022-05-04 23:21:00 +0300

    Update __init__.py

--- tgbot/middlewares/__init__.py
+++ tgbot/middlewares/__init__.py
@@ -6,7 +6,8 @@ from tgbot.middlewares.throttling import ThrottlingMiddleware
 
 
 # Подключение миддлварей
-def setup_middlwares(dp: Dispatcher):
-    dp.message.middleware(ThrottlingMiddleware())
-    dp.message.outer_middleware(ExistsUserMiddleware())
+def register_all_middlwares(dp: Dispatcher):
     dp.callback_query.outer_middleware(ExistsUserMiddleware())
+    dp.message.outer_middleware(ExistsUserMiddleware())
+
+    dp.message.middleware(ThrottlingMiddleware())
