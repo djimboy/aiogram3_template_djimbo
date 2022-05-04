0859da85c37817b52b190f78fae12dcdf08ea615
Djimbo <djimbosfan@mail.ru>
2022-05-04 23:14:16 +0300

    Update misc_functions.py

--- tgbot/utils/misc_functions.py
+++ tgbot/utils/misc_functions.py
@@ -2,25 +2,25 @@
 from aiogram import Bot
 from aiogram.types import FSInputFile
 
-from tgbot.config import get_admins, DATABASE_PATH
+from tgbot.config import get_admins, PATH_DATABASE
 from tgbot.utils.const_functions import get_date
 
 
 # Выполнение функции после запуска бота (рассылка админам о запуске бота)
-async def on_startup_notify(bot: Bot):
+async def startup_notify(bot: Bot):
     if len(get_admins()) >= 1:
         await send_admins(bot, "<b>✅ Бот был запущен</b>")
 
 
 # Автоматические бэкапы БД
-async def auto_backup(bot: Bot):
-    document = FSInputFile(DATABASE_PATH)
-
+async def autobackup(bot: Bot):
     for admin in get_admins():
-        await bot.send_document(admin,
-                                document,
-                                caption=f"<b>📦 AUTOBACKUP</b>\n"
-                                        f"<code>🕰 {get_date()}</code>")
+        try:
+            await bot.send_document(admin, FSInputFile(PATH_DATABASE),
+                                    caption=f"<b>📦 AUTOBACKUP</b>\n"
+                                            f"<code>🕰 {get_date()}</code>")
+        except:
+            pass
 
 
 # Отправка сообщения всем админам
