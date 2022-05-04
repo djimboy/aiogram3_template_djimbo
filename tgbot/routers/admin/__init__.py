53c4701398a63243cb99e80d4c3d60ed005e8913
Djimbo <djimbosfan@mail.ru>
2022-05-05 00:46:26 +0300

    Update __init__.py

--- tgbot/routers/admin/__init__.py
+++ tgbot/routers/admin/__init__.py
@@ -1,17 +1,9 @@
 # - *- coding: utf- 8 - *-
 from aiogram import Router
 
-from tgbot.handlers.admin.admin_menu import register_admin_menu
-from tgbot.handlers.main_start import register_main_start
-from tgbot.handlers.user import register_user_menu
-from tgbot.handlers.z_all_errors import register_all_errors
-from tgbot.handlers.z_all_missed import register_all_missed
+from tgbot.routers.admin.admin_menu import router_admin_menu
 
 
 # Подключение хендлеров для админа
-def setup_admin_handlers(router: Router):
-    register_all_errors(router)
-    register_main_start(router)
-    register_user_menu(router)
-    register_admin_menu(router)
-    register_all_missed(router)
+def setup_admin_handlers(admin_router: Router):
+    admin_router.include_router(router_admin_menu)
