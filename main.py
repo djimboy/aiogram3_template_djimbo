dc07a74c9e94ec2b4cb9f40ac38c970256589dc4
Djimbo <djimbosfan@mail.ru>
2022-05-04 23:09:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import asyncio
 import os
 import sys
 
-from aiogram import Bot, Dispatcher, Router, F
+from aiogram import Bot, Dispatcher
 
-from tgbot.config import BOT_TOKEN, scheduler, get_admins
-from tgbot.handlers.admin import setup_admin_handlers
-from tgbot.handlers.user import setup_user_handlers
-from tgbot.middlewares import setup_middlwares
+from tgbot.config import BOT_TOKEN, scheduler
+from tgbot.middlewares import register_all_middlwares
+from tgbot.routers import register_all_routes
 from tgbot.services.api_session import RequestsSession
 from tgbot.services.api_sqlite import create_bdx
 from tgbot.utils.misc.bot_commands import set_commands
 from tgbot.utils.misc.bot_logging import start_logging
-from tgbot.utils.misc_functions import auto_backup, on_startup_notify
+from tgbot.utils.misc_functions import autobackup, startup_notify
 
 
 # Запуск шедулеров
 async def scheduler_start(bot):
-    scheduler.add_job(auto_backup, "interval", seconds=43200, args=(bot,))  # Автобэкап каждые 43200 секунд
+    scheduler.add_job(autobackup, "cron", hour=00, args=(bot,))  # Автобэкап в 12:00
+    scheduler.add_job(autobackup, "cron", hour=12, args=(bot,))  # Автобэкап в 00:00
 
 
 # Запуск бота и функций
@@ -26,40 +26,24 @@ async def main():
     create_bdx()
 
     scheduler.start()
-
-    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+    dispatcher = Dispatcher()
     rSession = RequestsSession()
-
-    # Создание роутеров
-    admin_router = Router()
-    user_router = Router()
-
-    # Создание диспетчера и подключение роутеров
-    dp = Dispatcher()
-    dp.include_router(admin_router)
-    dp.include_router(user_router)
-
-    # Подключение мидлварей
-    setup_middlwares(dp)
-
-    # Подключение фильтров
-    admin_router.message.filter(F.from_user.id.in_(get_admins()) & F.chat.type == "private")
-    user_router.message.filter(F.chat.type == "private")
+    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 
     # Подключение хендлеров
-    setup_admin_handlers(admin_router)
-    setup_user_handlers(user_router)
+    register_all_middlwares(dispatcher)  # Регистрация всех мидлварей
+    register_all_routes(dispatcher)  # Регистрация всех роутеров
 
     try:
         await set_commands(bot)
-        await on_startup_notify(bot)
+        await startup_notify(bot)
         await scheduler_start(bot)
 
         print("~~~~~ Bot was started ~~~~~")
-        logger.info("Bot was started")
+        logger.warning("Bot was started")
 
         await bot.get_updates(offset=-1)
-        await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types(), rSession=rSession)
+        await dispatcher.start_polling(bot, allowed_updates=dispatcher.resolve_used_update_types(), rSession=rSession)
     finally:
         await rSession.close()
         await bot.session.close()
