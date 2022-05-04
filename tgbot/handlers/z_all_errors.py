85d58de5a21c12d1297612953c592396e9578a82
Djimbo <djimbosfan@mail.ru>
2022-05-04 23:17:31 +0300

    Update z_all_errors.py

--- tgbot/handlers/z_all_errors.py
+++ tgbot/handlers/z_all_errors.py
@@ -5,14 +5,13 @@ from aiogram.types import Update
 
 from tgbot.utils.misc.bot_logging import start_logging
 
+router_errors = Router()
+
 
 # Обработка ошибок
+@router_errors.errors()
 async def errors_handler(update: Update, exception: TelegramAPIError):
     start_logging().exception(
-        f"{str(exception)}.\n"
+        f"Exception: {str(exception)}\n"
         f"Update: {update.dict()}"
     )
-
-
-def register_all_errors(router: Router):
-    router.errors.register(errors_handler)
