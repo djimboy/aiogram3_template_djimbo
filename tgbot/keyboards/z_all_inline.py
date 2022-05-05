e32a0184f869b5e74812d613697dc5337a513507
Djimbo <djimbosfan@mail.ru>
2022-05-05 22:17:45 +0300

    Update z_all_inline.py

--- tgbot/keyboards/z_all_inline.py
+++ tgbot/keyboards/z_all_inline.py
@@ -3,11 +3,20 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from tgbot.utils.const_functions import ikb
 
-# Тестовые инлайн кнопки
-test_inl = InlineKeyboardBuilder(
+# Тестовые админ инлайн кнопки
+admin_inl = InlineKeyboardBuilder(
 ).row(
-    ikb("Test 1", data="..."),
-    ikb("Test 2", data="..."),
+    ikb("Admin Inline 1", data="..."),
+    ikb("Admin Inline 2", data="..."),
 ).row(
-    ikb("Test 3", data="..."),
+    ikb("Admin Inline 3", data="..."),
+).as_markup()
+
+# Тестовые юзер инлайн кнопки
+user_inl = InlineKeyboardBuilder(
+).row(
+    ikb("User Inline 1", data="..."),
+    ikb("User Inline 2", data="..."),
+).row(
+    ikb("User Inline 3", data="..."),
 ).as_markup()
