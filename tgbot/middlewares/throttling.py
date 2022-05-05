ac303f49bc37d2a245c7cdbdc47a49ec5a045dcb
Djimbo <djimbosfan@mail.ru>
2022-05-05 22:18:32 +0300

    Update throttling.py

--- tgbot/middlewares/throttling.py
+++ tgbot/middlewares/throttling.py
@@ -7,12 +7,11 @@ from aiogram.dispatcher.event.handler import HandlerObject
 
 # Антиспам
 class ThrottlingMiddleware(BaseMiddleware):
-    def __init__(self, default_rate: int = 0.5) -> None:
-        self.limiters = {}
-
+    def __init__(self, default_rate: int = 1) -> None:
+        self.last_throttled = int(time.time())
         self.default_rate = default_rate
-        self.count_throttled = 1
-        self.last_throttled = 0
+        self.now_rate = default_rate
+        self.count_throttled = 0
 
     async def __call__(self, handler, event: types.Message, data):
         real_handler: HandlerObject = data["handler"]
@@ -22,17 +21,24 @@ class ThrottlingMiddleware(BaseMiddleware):
             skip_pass = real_handler.flags.get("skip_pass")
 
         if skip_pass:
-            if int(time.time()) - self.last_throttled >= self.default_rate:
+            if int(time.time()) - self.last_throttled >= self.now_rate:
                 self.last_throttled = int(time.time())
-                self.default_rate = 0.5
+                self.now_rate = self.default_rate
                 self.count_throttled = 0
+
                 return await handler(event, data)
             else:
-                if self.count_throttled >= 2:
-                    self.default_rate = 3
-                else:
+                if self.count_throttled == 0:
                     self.count_throttled += 1
+                    self.now_rate = self.default_rate * 1.5
+
+                    return await handler(event, data)
+                elif self.count_throttled == 1:
+                    self.count_throttled += 1
+                    self.now_rate = self.default_rate * 2
                     await event.reply("<b>❗ Пожалуйста, не спамьте.</b>")
+                else:
+                    self.now_rate = 3
 
             self.last_throttled = int(time.time())
         else:
