7689d661dea6aaeac4aac15a08c4d8745e9f9e95
Djimbo <djimbosfan@mail.ru>
2022-05-05 22:18:00 +0300

    Update z_all_reply.py

--- tgbot/keyboards/z_all_reply.py
+++ tgbot/keyboards/z_all_reply.py
@@ -3,11 +3,20 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from tgbot.utils.const_functions import rkb
 
-# Тестовые реплай кнопки
-test_rep = ReplyKeyboardBuilder(
+# Тестовые админ реплай кнопки
+admin_rep = ReplyKeyboardBuilder(
 ).row(
-    rkb("User 1"),
-    rkb("User 2"),
+    rkb("Admin Reply 1"),
+    rkb("Admin Reply 2"),
 ).row(
-    rkb("User 3"),
+    rkb("⬅ Главное меню"),
+).as_markup(resize_keyboard=True)
+
+# Тестовые юзер реплай кнопки
+user_rep = ReplyKeyboardBuilder(
+).row(
+    rkb("User Reply 1"),
+    rkb("User Reply 2"),
+).row(
+    rkb("⬅ Главное меню"),
 ).as_markup(resize_keyboard=True)
