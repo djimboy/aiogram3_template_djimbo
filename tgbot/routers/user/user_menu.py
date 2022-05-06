7c26919ea47598c4134cab21967c1c9441f89ca1
Djimbo <djimbosfan@mail.ru>
2022-05-06 14:50:02 +0300

    Update user_menu.py

--- tgbot/routers/user/user_menu.py
+++ tgbot/routers/user/user_menu.py
@@ -1,33 +1,36 @@
 # - *- coding: utf- 8 - *-
-from aiogram import types, Router, Bot
+from aiogram import Router, Bot
 from aiogram.dispatcher.fsm.context import FSMContext
+from aiogram.types import Message
 
 from tgbot.keyboards.admin_inline import menu_finl
-from tgbot.keyboards.z_all_inline import test_inl
-from tgbot.keyboards.z_all_reply import test_rep
+from tgbot.keyboards.z_all_inline import user_inl
+from tgbot.keyboards.z_all_reply import user_rep
+from tgbot.services.api_session import RequestsSession as RS
+from tgbot.utils.misc.bot_models import UserDB
 
 router_user_menu = Router()
 
 
 # Кнопка - User Inline
 @router_user_menu.message(text="User Inline")
-async def user_button_inline(message: types.Message, bot: Bot, rSession, state: FSMContext):
+async def user_button_inline(message: Message, bot: Bot, state: FSMContext, rSession: RS, user: UserDB):
     await state.clear()
 
-    await message.answer("Click Button - User Inline", reply_markup=test_inl)
+    await message.answer("Click Button - User Inline", reply_markup=user_inl)
 
 
 # Кнопка - User Reply
 @router_user_menu.message(text="User Reply")
-async def user_button_reply(message: types.Message, bot: Bot, rSession, state: FSMContext):
+async def user_button_reply(message: Message, bot: Bot, state: FSMContext, rSession: RS, user: UserDB):
     await state.clear()
 
-    await message.answer("Click Button - User Reply", reply_markup=test_rep)
+    await message.answer("Click Button - User Reply", reply_markup=user_rep)
 
 
 # Команда - /inline
 @router_user_menu.message(commands="inline")
-async def user_command_inline(message: types.Message, bot: Bot, rSession, state: FSMContext):
+async def user_command_inline(message: Message, bot: Bot, state: FSMContext, rSession: RS, user: UserDB):
     await state.clear()
 
     await message.answer("Click command - /inline", reply_markup=menu_finl(message.from_user.id))
