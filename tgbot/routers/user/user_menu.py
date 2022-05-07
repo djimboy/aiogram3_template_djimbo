fde9c43a315e3df60619e3f4f58151e720639127
Djimbo <djimbosfan@mail.ru>
2022-05-07 13:24:45 +0300

    Update user_menu.py

--- tgbot/routers/user/user_menu.py
+++ tgbot/routers/user/user_menu.py
@@ -1,20 +1,18 @@
 # - *- coding: utf- 8 - *-
 from aiogram import Router, Bot
-from aiogram.dispatcher.fsm.context import FSMContext
 from aiogram.types import Message
 
 from tgbot.keyboards.admin_inline import menu_finl
 from tgbot.keyboards.z_all_inline import user_inl
 from tgbot.keyboards.z_all_reply import user_rep
-from tgbot.services.api_session import RequestsSession as RS
-from tgbot.utils.misc.bot_models import UserDB
+from tgbot.utils.misc.bot_models import UserDB, FSM, RS
 
 router_user_menu = Router()
 
 
 # Кнопка - User Inline
 @router_user_menu.message(text="User Inline")
-async def user_button_inline(message: Message, bot: Bot, state: FSMContext, rSession: RS, user: UserDB):
+async def user_button_inline(message: Message, bot: Bot, state: FSM, rSession: RS, user: UserDB):
     await state.clear()
 
     await message.answer("Click Button - User Inline", reply_markup=user_inl)
@@ -22,7 +20,7 @@ async def user_button_inline(message: Message, bot: Bot, state: FSMContext, rSes
 
 # Кнопка - User Reply
 @router_user_menu.message(text="User Reply")
-async def user_button_reply(message: Message, bot: Bot, state: FSMContext, rSession: RS, user: UserDB):
+async def user_button_reply(message: Message, bot: Bot, state: FSM, rSession: RS, user: UserDB):
     await state.clear()
 
     await message.answer("Click Button - User Reply", reply_markup=user_rep)
@@ -30,7 +28,7 @@ async def user_button_reply(message: Message, bot: Bot, state: FSMContext, rSess
 
 # Команда - /inline
 @router_user_menu.message(commands="inline")
-async def user_command_inline(message: Message, bot: Bot, state: FSMContext, rSession: RS, user: UserDB):
+async def user_command_inline(message: Message, bot: Bot, state: FSM, rSession: RS, user: UserDB):
     await state.clear()
 
     await message.answer("Click command - /inline", reply_markup=menu_finl(message.from_user.id))
