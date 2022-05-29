66c00106dbb0ea61db6fb0137c1e578f0832992f
Djimbo <djimbosfan@mail.ru>
2022-05-29 04:32:25 +0300

    Update bot_logging.py

--- tgbot/utils/misc/bot_logging.py
+++ tgbot/utils/misc/bot_logging.py
@@ -1,32 +1,32 @@
 # - *- coding: utf- 8 - *-
-import logging
+import logging as bot_logger
 
 import colorlog
 
-
 from tgbot.config import PATH_LOGS
 
+# Формат логгирования
+log_formatter_file = bot_logger.Formatter("%(levelname)s | %(asctime)s | %(filename)s:%(lineno)d | %(message)s")
+log_formatter_console = colorlog.ColoredFormatter(
+    "%(purple)s%(levelname)s %(blue)s|%(purple)s %(asctime)s %(blue)s|%(purple)s %(filename)s:%(lineno)d %(blue)s|%(purple)s %(message)s%(red)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+)
 
-# Создание логгирования
-def start_logging():
-    log_formatter_file = logging.Formatter("%(levelname)s | %(asctime)s | %(filename)s:%(lineno)d | %(message)s")
-    log_formatter_console = colorlog.ColoredFormatter(
-        "%(purple)s%(levelname)s %(blue)s|%(purple)s %(asctime)s %(blue)s|%(purple)s %(filename)s:%(lineno)d %(blue)s|%(purple)s %(message)s%(red)s",
-        datefmt="%d-%m-%Y %H:%M:%S",
-    )
+# Логгирование в файл logs.log
+file_handler = bot_logger.FileHandler(PATH_LOGS, "w", "utf-8")
+file_handler.setFormatter(log_formatter_file)
+file_handler.setLevel(bot_logger.INFO)
 
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.INFO)
+# Логгирование в консоль
+console_handler = bot_logger.StreamHandler()
+console_handler.setFormatter(log_formatter_console)
+console_handler.setLevel(bot_logger.CRITICAL)
 
-    file_handler = logging.FileHandler(PATH_LOGS, "w", "utf-8")
-    file_handler.setFormatter(log_formatter_file)
-    file_handler.setLevel(logging.INFO)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter_console)
-    console_handler.setLevel(logging.ERROR)
-
-    root_logger.addHandler(file_handler)
-    root_logger.addHandler(console_handler)
-
-    return root_logger
+# Подключение настроек логгирования
+bot_logger.basicConfig(
+    format="%(levelname)s | %(asctime)s | %(filename)s:%(lineno)d | %(message)s",
+    handlers=[
+        file_handler,
+        console_handler
+    ]
+)
