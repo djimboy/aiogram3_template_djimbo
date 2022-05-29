807b00147758ea46261ccbaaa94a45e41b37761c
Djimbo <djimbosfan@mail.ru>
2022-05-29 04:50:04 +0300

    Update config.py

--- tgbot/config.py
+++ tgbot/config.py
@@ -3,23 +3,25 @@ import configparser
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+from tgbot.utils.const_functions import clear_list
+
 scheduler = AsyncIOScheduler()
 
-config = configparser.ConfigParser()
-config.read("settings.ini")
+read_config = configparser.ConfigParser()
+read_config.read("settings.ini")
 
-BOT_TOKEN = config["settings"]["token"].strip()
+BOT_TOKEN = read_config["settings"]["token"].strip()
 PATH_DATABASE = "tgbot/data/database.db"  # Путь к БД
 PATH_LOGS = "tgbot/data/logs.log"  # Путь к Логам
 
 
 # Получение администраторов бота
 def get_admins():
-    config = configparser.ConfigParser()
-    config.read("settings.ini")
-    admins = config["settings"]["admin_id"].strip()
+    read_admins = configparser.ConfigParser()
+    read_admins.read("settings.ini")
+
+    admins = read_admins["settings"]["admin_id"].strip()
     admins = admins.replace(" ", "")
-    save_admins = []
 
     if "," in admins:
         admins = admins.split(",")
@@ -28,14 +30,7 @@ def get_admins():
             admins = [admins]
         else:
             admins = []
-            print("***** ENTER ADMIN ID IN SETTINGS.ini *****")
 
-    if "" in admins:
-        admins.remove("")
-    if " " in admins:
-        admins.remove(" ")
+    admins = list(map(int, clear_list(admins)))
 
-    for admin in admins:
-        save_admins.append(int(admin))
-
-    return save_admins
+    return admins
