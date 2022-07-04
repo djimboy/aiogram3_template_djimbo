89ad212e9e1a08feaf6c566cff574788e059199b
Djimbo <djimbosfan@mail.ru>
2022-07-04 03:07:01 +0300

    Update admin_reply.py

--- tgbot/keyboards/admin_reply.py
+++ tgbot/keyboards/admin_reply.py
@@ -9,12 +9,14 @@ from tgbot.utils.const_functions import rkb
 def menu_frep(user_id):
     keyboard = ReplyKeyboardBuilder(
     ).row(
-        rkb("User Inline"), rkb("User Reply"),
+        rkb("User Inline"),
+        rkb("User Reply"),
     )
 
     if user_id in get_admins():
         keyboard.row(
-            rkb("Admin Inline"), rkb("Admin Reply"),
+            rkb("Admin Inline"),
+            rkb("Admin Reply"),
         )
 
     return keyboard.as_markup(resize_keyboard=True)
